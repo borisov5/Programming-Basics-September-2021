00aa3836670368989729c9d088889efb4ae6038d
Ivelin Borisov <90949939+borisov5@users.noreply.github.com>
2022-05-20 21:17:11 +0300

    Add files via upload

---
+++ Checks/bonus_tasks.py
@@ -0,0 +1,14 @@
+#на терминала пиша pip install matplotlib
+#import matplotlib.pylab as p
+#s = [35, 25, 20, 10, 10]
+#k = ['Python', 'Java', 'C#', 'C', 'GO']
+#p.pie(s, labels=k)
+#p.show()
+
+#на терминала пиша pip install pyqrcode, pypng
+#import pyqrcode
+#import png
+#from pyqrcode import QRCode
+#link = "PASTE LINK HERE"
+#url = pyqrcode.create(link)
+#url.png('qr.png', scale=8)
